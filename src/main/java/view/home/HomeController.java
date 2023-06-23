267fd3a95072c86c65c14837867444085abf9d66
2uanDM <hokage321xxx@gmail.com>
2023-06-23 17:13:23 +0700

    feat: add welcoming sound

--- src/main/java/view/home/HomeController.java
+++ src/main/java/view/home/HomeController.java
@@ -1,9 +1,29 @@
 package main.java.view.home;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.media.*;
 
-public class HomeController {
+public class HomeController implements Initializable {
+
+    @FXML
+    private Button aboutButton;
+
+    @FXML
+    private Button helpButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rBundle) {
+        // Welcome sound
+        AudioClip welcomeSound = new AudioClip(
+                this.getClass().getResource("../../../resources/sound/chime.wav").toString());
+        welcomeSound.setVolume(0.5);
+        welcomeSound.play();
+    }
 
 }
