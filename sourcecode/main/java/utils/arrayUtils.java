a185b9b7b79d699efc7fa54779806e98fd9700c7
2uanDM <hokage321xxx@gmail.com>
2023-07-09 21:58:35 +0700

    rename some files

--- sourcecode/main/java/utils/arrayUtils.java
+++ sourcecode/main/java/utils/arrayUtils.java
@@ -1,35 +1,35 @@
 package utils;
 
 // We provide 3 basic static methods for Array includes finding maximun, minimun element and resizing the array length.
-public abstract class arrayUtils {
+public abstract class ArrayUtils {
     public static int max(int[] array) {
         int maxElement = array[0];
-        for (int i: array) {
+        for (int i : array) {
             if (i > maxElement) {
                 maxElement = i;
             }
-            //else: continue iterate in for loop
+            // else: continue iterate in for loop
         }
-        //here, we do not consider whether or not the maximun element is unique
+        // here, we do not consider whether or not the maximun element is unique
         return maxElement;
     }
 
     public static int min(int[] array) {
         int minElement = array[0];
-        for (int i: array) {
+        for (int i : array) {
             if (i < minElement) {
                 minElement = i;
             }
-            //else: continue iterate in the for loop
+            // else: continue iterate in the for loop
         }
         return minElement;
     }
 
     public static int[] Resize(int[] array) {
         int oldLength = array.length;
-        int newLength = oldLength * 2; //double the length
+        int newLength = oldLength * 2; // double the length
         int[] newArray = new int[newLength];
-        //copy values to the new array
+        // copy values to the new array
         for (int i = 0; i < oldLength; i++) {
             newArray[i] = array[i];
         }
@@ -38,9 +38,9 @@ public abstract class arrayUtils {
 
     public static int[][] Resize(int[][] array) {
         int oldLength = array.length;
-        int newLength = oldLength * 2; //double the length
+        int newLength = oldLength * 2; // double the length
         int[][] newArray = new int[newLength][array[0].length];
-        //copy values to the new array
+        // copy values to the new array
         for (int i = 0; i < oldLength; i++) {
             newArray[i] = array[i];
         }
@@ -49,14 +49,13 @@ public abstract class arrayUtils {
 
     public static String[] Resize(String[] array) {
         int oldLength = array.length;
-        int newLength = oldLength * 2; //double the length
+        int newLength = oldLength * 2; // double the length
         String[] newArray = new String[newLength];
-        //copy values to the new array
+        // copy values to the new array
         for (int i = 0; i < oldLength; i++) {
             newArray[i] = array[i];
         }
         return newArray;
     }
 
-    
 }
