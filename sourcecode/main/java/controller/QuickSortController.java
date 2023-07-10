6b549304328630a7e9b2ba9fed34d032534cde44
2uanDM <hokage321xxx@gmail.com>
2023-07-11 04:03:13 +0700

    done: SortView

--- sourcecode/main/java/controller/QuickSortController.java
+++ sourcecode/main/java/controller/QuickSortController.java
@@ -5,31 +5,30 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 
 public class QuickSortController extends SortController {
 
-    private void setVisible(Node component, boolean status) {
+    private double getComponentWidth(Node component) {
         if (component instanceof Button) {
             Button button = (Button) component;
-            button.setVisible(status);
+            return button.getWidth();
         } else if (component instanceof TextField) {
             TextField textField = (TextField) component;
-            textField.setVisible(status);
+            return textField.getWidth();
         } else if (component instanceof TextArea) {
             TextArea textArea = (TextArea) component;
-            textArea.setVisible(status);
-        } else if (component instanceof ImageView) {
-            ImageView imageView = (ImageView) component;
-            imageView.setVisible(status);
+            return textArea.getWidth();
+        } else {
+            // Handle other component types if needed
+            return 0.0;
         }
     }
 
     public void onAction(ActionEvent e) {
         if (e.getTarget() instanceof Node) {
             Node targetNode = (Node) e.getTarget();
-            setVisible(targetNode, false);
-            System.out.println("done");
+            double width = getComponentWidth(targetNode);
+            System.out.println("Width: " + width);
         }
     }
 }
