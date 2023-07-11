b6d71b178dfe2cef59ef4c0c7803d89dc4f56941
2uanDM <hokage321xxx@gmail.com>
2023-07-11 22:15:56 +0700

    update ColumnBar: swap

--- sourcecode/main/java/model/object/ColumnBar.java
+++ sourcecode/main/java/model/object/ColumnBar.java
@@ -1,14 +1,17 @@
 package main.java.model.object;
 
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 public class ColumnBar extends Rectangle {
     public static final double MAX_HEIGHT = 250.0;
     public static final double MIN_HEIGHT = 30.0;
-    public static final double COL_WIDTH = 35.0;
+    public static final double COL_WIDTH = 40.0;
     public static final double MAX_VALUE = 50.0;
     public static final double MIN_VALUE = 0.0;
 
@@ -31,7 +34,20 @@ public class ColumnBar extends Rectangle {
         this.setFill(color);
     }
 
-    // public swap(ColumnBar column)
+    public void swap(ColumnBar columnBar, double duration) {
+        double currentX = this.getLayoutX();
+        double otherX = columnBar.getLayoutX();
+
+        TranslateTransition thisTransition = new TranslateTransition(Duration.seconds(duration), this);
+        thisTransition.setToX(otherX - currentX);
+        thisTransition.setInterpolator(Interpolator.EASE_BOTH);
+        thisTransition.play();
+
+        TranslateTransition otherTransition = new TranslateTransition(Duration.seconds(duration), columnBar);
+        otherTransition.setToX(currentX - otherX);
+        otherTransition.setInterpolator(Interpolator.EASE_BOTH);
+        otherTransition.play();
+    }
 
     private double getHeight(double value) {
         return value / MAX_VALUE * (MAX_HEIGHT - MIN_HEIGHT) + MIN_HEIGHT;
